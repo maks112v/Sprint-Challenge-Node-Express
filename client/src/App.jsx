7afae23cac5c0a72739a8f62100aeae09532a644
Maksim V <maks112v@gmail.com>
2019-03-22 13:04:40 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,42 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class App extends Component {
+  state = {
+    loading: true,
+    data: [],
+  };
   componentDidMount() {
     axios
       .get("http://localhost:4000/api/project/")
       .then(res => {
         console.log(res);
+        this.setState({
+          loading: false,
+          data: res.data,
+        });
       })
-      .error(err => {
+      .catch(err => {
         console.log(err);
       });
   }
   render() {
-    return <div />;
+    if (this.state.loading) {
+      return (
+        <div>
+          <h2>Loading</h2>
+        </div>
+      );
+    }
+    return (
+      <div>
+        {this.state.data.map(task => (
+          <div key={task.id}>
+            <h2>{task.name}</h2>
+            <h3>{task.description}</h3>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
